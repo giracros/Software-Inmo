915b9e7819d63d8c6d9c9a5d39fa57af997b09c0
danielfuentes <giracros@gmail.com>
2014-12-12 02:41:20 -0500

    Final :D 12 diciembre de 2014

--- Inmobiliaria/target/generated-sources/annotations/com/lenticulares/inmobiliaria/TZona_.java
+++ Inmobiliaria/target/generated-sources/annotations/com/lenticulares/inmobiliaria/TZona_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-12-10T21:00:29")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-12-12T02:29:51")
 @StaticMetamodel(TZona.class)
 public class TZona_ { 
 
